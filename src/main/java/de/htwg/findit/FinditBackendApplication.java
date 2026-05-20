3371e1b0e00caa5db8bd96d517cf7ee15eda65a6
Haris Melunovic <ha121mel@htwg-konstanz.de>
2026-05-20 20:17:29 +0200

    Initialize Spring Boot backend

---
+++ src/main/java/de/htwg/findit/FinditBackendApplication.java
@@ -0,0 +1,13 @@
+package de.htwg.findit;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FinditBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FinditBackendApplication.class, args);
+	}
+
+}
---
+++ src/test/java/de/htwg/findit/FinditBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package de.htwg.findit;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FinditBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
